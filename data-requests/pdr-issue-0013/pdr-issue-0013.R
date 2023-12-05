5e5a64ae37bf43eb10bfb04e515e9e8685b486d9
William Ridgeway <10358980+wrridgeway@users.noreply.github.com>
2023-12-05 15:45:53 -0600

    Share 2023 alt CDUs (#14)
    * First draft
    
    * Update .gitignore
    
    Co-authored-by: Dan Snow <31494343+dfsnow@users.noreply.github.com>
    
    * Update data-requests/pdr-issue-0013/pdr-issue-0013.R
    
    Co-authored-by: Dan Snow <31494343+dfsnow@users.noreply.github.com>
    
    * Comments
    
    ---------
    
    Co-authored-by: sweatyhandshake <wridgeway@cookcountyassessor.com>
    Co-authored-by: Dan Snow <31494343+dfsnow@users.noreply.github.com>

---
+++ data-requests/pdr-issue-0013/pdr-issue-0013.R
@@ -0,0 +1,49 @@
+# See https://github.com/ccao-data/public/issues/13
+library(aws.s3)
+library(DBI)
+library(dplyr)
+library(noctua)
+library(openxlsx)
+library(purrr)
+library(readr)
+
+# Local output path
+out_path <- "pdr-issue-0013.xlsx"
+
+# user16 descriptions
+file <- "Alternate CDU fields - dweldat, oby, comdat.xlsx"
+
+# Gather all 2023 parcels with non-NULL values for user16 (alt CDU) across the
+# three iasWorld tables that contain that column and join descriptions for each
+# value of user16 we're privy to.
+left_join(
+  # Gather alternate CDUs from comdat, oby, and dweldat
+  dbGetQuery(
+    conn = dbConnect(noctua::athena()),
+    read_file("pdr-issue-0013.sql")
+  ),
+  # Ingest and consolidate CDU descriptions
+  map(
+    getSheetNames(file),
+    function(x) {
+      read.xlsx(file, sheet = x, startRow = 2) |>
+        select(Value.2, Long.Desc)
+    }
+  ) |>
+    bind_rows() |>
+    distinct() |>
+    mutate(Long.Desc = paste0(Long.Desc, collapse = ", "), .by = Value.2) |>
+    distinct(),
+  by = join_by(user16 == Value.2)
+) |>
+  rename("alt cdu" = "user16", "alt cdu description" = "Long.Desc") |>
+  write.xlsx(out_path)
+
+# Upload to s3
+aws.s3::put_object(
+  out_path,
+  paste0("s3://ccao-data-public-us-east-1/", out_path)
+  )
+
+# Remove local file
+file.remove(out_path)
