79136a653977d8d3f466fada762c132c26baeaa5
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-02-05 17:50:58 +0000

    Commenting

--- data-requests/pdr-issue-0017/pdr-issue-0017.R
+++ data-requests/pdr-issue-0017/pdr-issue-0017.R
@@ -20,15 +20,15 @@ eav22 <- DBI::dbGetQuery(
   pull(pin)
 
 # Use tax_bill() to obtain EAVs for all '22 parcels, filter our rows that don't
-# pertain to a parcel's municipality, then aggregate by class and municipality.
-# It's worth noting here that this sample will NOT include all '22 parcels due
-# to this filter - any parcels that don't include a "muni" component in their
-# tax bill will not show up in this output
+# pertain to a parcel's municipality. It's worth noting here that this sample
+# will NOT include all '22 parcels due to this filter - any parcels that don't
+# include a "muni" component in their tax bill will not show up in this output
 munis <- tax_bill(year_vec = 2022, pin_vec = eav22) %>%
   filter(agency_minor_type == "MUNI") %>%
   mutate("Major Class" = substr(class, 1, 1)) %>%
   rename("Minor Class" = class)
 
+# Aggregate by class and municipality, output
 c("Minor Class", "Major Class") %>%
   set_names() %>%
   map(function(x) {
